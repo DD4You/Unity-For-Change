506228204fc949cc80b530aa4d7b5e40b5ed85ee
Vinay Rajput <codewithvinaysingh@gmail.com>
2023-05-08 11:53:55 +0530

    First Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="heading">Unity For Change</h1>
+      <div className="text-red-600">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  )
+}
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+const colors = require('tailwindcss/colors')
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.jsx'],
+  theme: {
+    extend: { colors },
+  },
+  plugins: [],
+}
+
